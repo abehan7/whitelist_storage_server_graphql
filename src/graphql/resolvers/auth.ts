739e7be52cb7ba8bc29690e87ed50842454511e6
abehan7 <hanjk13262@gmail.com>
2022-06-17 00:51:34 +0900

    add:> caver add

--- src/graphql/resolvers/auth.ts
+++ src/graphql/resolvers/auth.ts
@@ -1,15 +1,22 @@
 import { db } from "./../../models";
-import Web3 from "web3";
-const web3 = new Web3("https://kaikas.cypress.klaytn.net:8651/");
+import Caver, { SignatureData } from "caver-js";
+
+const caver = new Caver("https://kaikas.cypress.klaytn.net:8651/");
+type ISignatureData = object | SignatureData | string[];
+
 const resolvers = {
   Query: {
-    authenticate: async (_: any, args: { signer: string }) => {
-      const { signer } = args;
-      const wallet_address = web3.eth.accounts.recover(
-        "Message to sign",
-        signer
-      );
-      const doc = await db.User.findOne({ wallet_address });
+    authenticate: async (_: any, args: { signature: ISignatureData }) => {
+      // const signature = await caver.klay.sign(message, address);
+      const { signature } = args;
+      const publicKey = caver.utils.recoverPublicKey("sign message", signature);
+      const wallet_address = caver.klay.getAccountKey(publicKey);
+      console.log(wallet_address);
+      // const wallet_address = web3.eth.accounts.recover(
+      //   "Message to sign",
+      //   signer
+      // );
+      // const doc = await db.User.findOne({ wallet_address });
       // 이제 여기서 토큰만들어서 보내주기
       return true;
       // 여기서 signer가지고 와서 인증하기
